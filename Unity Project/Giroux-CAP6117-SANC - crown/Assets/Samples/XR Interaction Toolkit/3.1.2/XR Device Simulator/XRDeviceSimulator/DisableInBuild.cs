5210ede8577864b8396304e73e14fcf5d9d976b0
Samuel Watts <wattssamc@gmail.com>
2025-07-05 15:55:32 -0400

    Disable in headset keep in desktop

---
+++ Unity Project/Giroux-CAP6117-SANC - crown/Assets/Samples/XR Interaction Toolkit/3.1.2/XR Device Simulator/XRDeviceSimulator/DisableInBuild.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableInBuild : MonoBehaviour
+{
+    void Awake()
+    {
+#if !UNITY_EDITOR
+        gameObject.SetActive(false);
+#endif
+    }
+}
