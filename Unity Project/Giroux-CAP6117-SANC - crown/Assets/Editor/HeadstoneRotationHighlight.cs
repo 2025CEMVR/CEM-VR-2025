a4deb4186f8d0e67292b654ced576dec669eec1e
A-Brightman <161653283+A-Brightman@users.noreply.github.com>
2025-07-30 16:53:49 -0400

    rotated headstones (hopefully for last time) and added LOD (also hopefully for last time)

--- Unity Project/Giroux-CAP6117-SANC - crown/Assets/Editor/HeadstoneRotationHighlight.cs
+++ Unity Project/Giroux-CAP6117-SANC - crown/Assets/Editor/HeadstoneRotationHighlight.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class HeadstoneRotationHighlight : MonoBehaviour
+[InitializeOnLoad]
+public class HeadstoneRotationHighlight
 {
-    // Start is called before the first frame update
-    void Start()
+    private static readonly string prefabPath = "Assets/Prefabs/HSbundle.prefab";
+    private static GameObject targetPrefab;
+    private static bool isHighlightingEnabled = true;
+
+    //Gets the prefab and ensures it exists!
+    static HeadstoneRotationHighlight()
     {
-        
+        targetPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+
+        if (targetPrefab == null)
+        {
+            Debug.LogError($"Prefab not found at path: {prefabPath}");
+            return;
+        }
+        SceneView.duringSceneGui += OnSceneGUI;
     }
 
-    // Update is called once per frame
-    void Update()
+    [MenuItem("Tools/Toggle Headstone Highlight")]
+    private static void ToggleHighlighting()
     {
-        
+        isHighlightingEnabled = !isHighlightingEnabled;
+        Debug.Log($"Headstone Highlighting {(isHighlightingEnabled ? "Enabled" : "Disabled")}");
+        SceneView.RepaintAll();
+    }
+
+    //words and such
+    private static void OnSceneGUI(SceneView sceneView)
+    {
+        if (!isHighlightingEnabled || targetPrefab == null) return;
+
+        foreach (GameObject obj in GameObject.FindObjectsOfType<GameObject>())
+        {
+            if (PrefabUtility.GetPrefabInstanceStatus(obj) == PrefabInstanceStatus.Connected)
+            {
+                GameObject prefabRoot = PrefabUtility.GetCorrespondingObjectFromSource(obj);
+
+                if (prefabRoot == targetPrefab && Mathf.Abs(obj.transform.eulerAngles.y) != 0)
+                {
+                    Handles.color = Color.red;
+                    Handles.DrawWireCube(obj.transform.position, Vector3.one);
+
+                }
+            }
+        }
+
+        // Force the SceneView to repaint and update
+        SceneView.RepaintAll();
     }
 }
